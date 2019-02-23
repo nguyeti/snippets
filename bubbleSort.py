08f9f054ddc5c2ae24d027f06943ed272297d92b
nguyeti <nguyen.timothee@gmail.com>
2019-02-23 10:33:54 -0500

    Add bubble sort code

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+input = [1,5,2,604,98]
+
+def bubbleSort(array):
+  for i in range(len(array)):
+    for j in range(len(array) - 1):
+      if array[j] > array[j + 1]:
+        print "Swapping {0} and {1}".format(array[j], array[j+1])
+        temp = array[j+1]
+        array[j+1] = array[j]
+        array[j] = temp
+      print "[{0},{1}]: {2}".format(i, j, array)
+  print "Sorted array: {0}".format(array)
+
+bubbleSort(input)
