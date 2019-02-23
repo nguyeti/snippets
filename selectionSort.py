040e3b328106ceb0812b663e3b4dbd3e944ee7fc
nguyeti <nguyen.timothee@gmail.com>
2019-02-23 16:59:36 -0500

    Add output result

--- selectionSort.py
+++ selectionSort.py
@@ -5,19 +5,22 @@
 input = [10,5,2,604,98,2]
 
 def selectionSort(input):
-    print("The input is {0}".format(input))
-    for i in range(len(input)):
-        iMin = i
-        print("iMin: {0}".format(iMin))
-        for j in range(iMin + 1, len(input)):
-            if input[j] <= input[iMin]:
-                print("Swapping {0} and {1}".format(input[j], input[iMin]))
-                temp = input[iMin]
-                input[iMin] = input[j]
-                input[j] = temp
-                print(input)
-        print("The smallest number is at the leftmost position: {0}".format(iMin))
-        
-    print("Sorted array: {0}".format(input))
-        
+   for fillslot in range(len(input)-1,0,-1):
+        positionOfMax=0
+        for location in range(1,fillslot+1):
+            if input[location]>input[positionOfMax]:
+                positionOfMax = location
+
+        temp = input[fillslot]
+        input[fillslot] = input[positionOfMax]
+        input[positionOfMax] = temp
+        print("fillslot: {0}, array: {1}".format(fillslot, input))
 selectionSort(input)
+
+
+# Output
+# fillslot: 5, array: [10, 5, 2, 2, 98, 604]
+# fillslot: 4, array: [10, 5, 2, 2, 98, 604]
+# fillslot: 3, array: [2, 5, 2, 10, 98, 604]
+# fillslot: 2, array: [2, 2, 5, 10, 98, 604]
+# fillslot: 1, array: [2, 2, 5, 10, 98, 604]
