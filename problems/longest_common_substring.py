0b1829172ad9e5aa482b4a180de395c221f170c7
Timothée Nguyen <nguyen.timothee@gmail.com>
2020-06-29 11:25:54 -0400

    Add files via upload

---
+++ problems/longest_common_substring.py
@@ -0,0 +1,26 @@
+def lcs(S,T):
+    m = len(S)
+    n = len(T)
+    counter = [[0]*(n+1) for x in range(m+1)]
+    longest = 0
+    lcs_set = set()
+    for i in range(m):
+        for j in range(n):
+            if S[i] == T[j]:
+                c = counter[i][j] + 1
+                counter[i+1][j+1] = c
+                if c > longest:
+                    lcs_set = set()
+                    longest = c
+                    lcs_set.add(S[i-c+1:i+1])
+                elif c == longest:
+                    lcs_set.add(S[i-c+1:i+1])
+
+    return lcs_set
+
+# test 1
+ret = lcs('academy', 'abracadabra')
+for s in ret:
+    print(s)
+
+    # https://www.bogotobogo.com/python/python_longest_common_substring_lcs_algorithm_generalized_suffix_tree.php
